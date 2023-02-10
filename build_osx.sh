f894a1ab1b9affd7d9d05a79e1cb830239a26132
nickcastel50 <nickcastel50@gmail.com>
2023-02-10 10:31:04 -0500

    * updated mac os script to use make instead of gmake (which is possibly an alias)

--- build_osx.sh
+++ build_osx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #add path to emsdk environment below
-source /Users/example/path/GitHub/emsdk/emsdk_env.sh
+source /Users/soar/Documents/GitHub/emsdk/emsdk_env.sh
 ./macos_genie/genie gmake
 
 if [ "$1" != "" ];
@@ -9,15 +9,15 @@ then
 	if [ "$1" = "debug" ] 
 	then
 		cd gmake
-		gmake config=debugemscripten conway_geom_native
-		gmake config=debugemscripten conway_geom_native_mt
+		make config=debugemscripten conway_geom_native
+		make config=debugemscripten conway_geom_native_mt
 	fi
 
 	if [ "$1" = "release" ]
 	then
 		cd gmake
-		gmake config=releaseemscripten conway_geom_native
-		gmake config=releaseemscripten conway_geom_native_mt
+		make config=releaseemscripten conway_geom_native
+		make config=releaseemscripten conway_geom_native_mt
 	fi
 fi
 
