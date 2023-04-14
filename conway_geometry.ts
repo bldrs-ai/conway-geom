dffff834c25e7ff86be0d6305a3f0d1ff144210e
nickcastel50 <nickcastel50@gmail.com>
2023-04-14 14:42:40 -0400

    * Removed semicolons from conway_geometry.ts

--- conway_geometry.ts
+++ conway_geometry.ts
@@ -1,82 +1,82 @@
 
-import { default as conway_geom_wasm } from "./conway_geom_wasm.js";
+import { default as conway_geom_wasm } from "./conway_geom_wasm.js"
 
 
 
 export interface GeometryObject {
-    GetVertexData: () => any;
-    GetVertexDataSize: () => number;
-    GetIndexData: () => any;
-    GetIndexDataSize: () => number;
+    GetVertexData: () => any
+    GetVertexDataSize: () => number
+    GetIndexData: () => any
+    GetIndexDataSize: () => number
     AddGeometry(parameter: GeometryObject): void
 }
 
 export interface ParamsPolygonalFaceSet {
-    numPoints: number;
-    numIndices: number;
-    indicesPerFace: number;
-    indexedPolygonalFaceWithVoids: boolean;
-    points: any;
-    indices: number[];
+    numPoints: number
+    numIndices: number
+    indicesPerFace: number
+    indexedPolygonalFaceWithVoids: boolean
+    points: any
+    indices: number[]
 }
 
 export interface ResultsGltf {
-    success: boolean;
-    bufferUris: any;
-    buffers: any;
+    success: boolean
+    bufferUris: any
+    buffers: any
 }
 
-export type LocateFileHandlerFn = (path: string, prefix: string) => string;
+export type LocateFileHandlerFn = (path: string, prefix: string) => string
 
 export class ConwayGeometry {
-    modelId: number = -1;
-    //private module: any;
+    modelId: number = -1
+    //private module: any
 
-    public wasmModule: undefined | any = undefined;
-    fs: undefined | any = undefined;
-    wasmPath: string = "";
-    isWasmPathAbsolute = false;
-    initialized = false;
+    public wasmModule: undefined | any = undefined
+    fs: undefined | any = undefined
+    wasmPath: string = ""
+    isWasmPathAbsolute = false
+    initialized = false
 
     constructor() {
     }
 
     async initialize() {
         if (!this.wasmModule) {
-            this.wasmModule = await new conway_geom_wasm();
+            this.wasmModule = await new conway_geom_wasm()
         }
-        //console.log(this.wasmModule);
-        this.modelId = this.wasmModule.InitializeGeometryProcessor();
+        //console.log(this.wasmModule)
+        this.modelId = this.wasmModule.InitializeGeometryProcessor()
 
-        //console.log("modelId: " + this.modelId);
+        //console.log("modelId: " + this.modelId)
 
-        this.initialized = true;
+        this.initialized = true
     }
 
     addGeometry(parameter: GeometryObject) {
-        this.wasmModule.AddGeometry(parameter);
+        this.wasmModule.AddGeometry(parameter)
     }
 
     getGeometry(parameters: ParamsPolygonalFaceSet): GeometryObject {
-        const result = this.wasmModule.GetGeometry(this.modelId, parameters);
-        return result;
+        const result = this.wasmModule.GetGeometry(this.modelId, parameters)
+        return result
     }
 
     toGltf(geometry: GeometryObject, isGlb: boolean, outputDraco: boolean, fileUri: string): ResultsGltf {
-        return this.wasmModule.GeometryToGltf(this.modelId, geometry, isGlb, outputDraco, fileUri);
+        return this.wasmModule.GeometryToGltf(this.modelId, geometry, isGlb, outputDraco, fileUri)
     }
 
     toObj(geometry: GeometryObject): string {
-        return this.wasmModule.GeometryToObj(this.modelId, geometry, 0);
+        return this.wasmModule.GeometryToObj(this.modelId, geometry, 0)
     }
 
     myTestFunction(param: any): any {
-        return this.wasmModule.myTestFunction(param);
+        return this.wasmModule.myTestFunction(param)
     }
 
     destroy() {
-        this.wasmModule.FreeGeometryProcessor(this.modelId);
-        this.modelId = -1;
+        this.wasmModule.FreeGeometryProcessor(this.modelId)
+        this.modelId = -1
     }
 }
 
