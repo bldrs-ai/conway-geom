5119a12976dfe7ca31dbcb391db719cf03ecc943
nickcastel50 <nickcastel50@gmail.com>
2023-04-23 18:03:20 -0400

    * updated build_win.bat with Ogali's changes from Mac script

--- build_win.bat
+++ build_win.bat
@@ -1,30 +1,65 @@
 @echo off
+setlocal enabledelayedexpansion
 
-set EMSDKPATH=%EMSDK%
-set FILENAME="\emsdk_env.bat"
-
-rem Combine two paths
-set COMBINED="%EMSDK:"=%\%FILENAME:"=%"
-call %COMBINED%
-call windows_genie\genie gmake
-
-if "%1" NEQ "" (
-if "%1" == "debug" (
-cd gmake
-	make config=debug64 conway_geom_native
-	make config=debug64 webifc_native
-	make config=debugemscripten conway_geom_wasm
-	
-	cd ..
+if "%1"=="" (
+    echo Usage: %0 ^<clean ^| debug ^| release^>
+    exit /b 1
 )
 
-if "%1" == "release" (
-	cd gmake
-	make config=release64 conway_geom_native
-	make config=release64 webifc_native
-	make config=releaseemscripten conway_geom_wasm
-	
-	cd ..
-))
+if not defined EMSDK (
+    echo ! EMSDK environment variable not defined
+    echo ! Did you forget to activate the emsdk environment?
+    echo !! See https://emscripten.org/docs/getting_started/downloads.html
+    exit /b 1
+)
 
-echo Finished.
+call "%EMSDK%\emsdk_env.bat"
+set EMSCRIPTEN=%EMSDK%\upstream\emscripten
+
+if not exist ".git\modules" (
+    git submodule update --init
+    if errorlevel 1 (
+        echo ! Could not initialize Git submodules
+        exit /b 1
+    )
+)
+
+.\windows_genie\genie gmake
+if errorlevel 1 (
+    echo ! Could not generate makefiles
+    exit /b 1
+)
+
+if /i "%1"=="clean" (
+    cd gmake && (
+        make config=debug64 clean && make config=debugemscripten clean && make config=release64 clean && make config=releaseemscripten clean
+    )
+    if errorlevel 1 (
+        exit /b 1
+    ) else (
+        exit /b 0
+    )
+) else if /i "%1"=="debug" (
+    set native_config=debug64
+    set wasm_config=debugemscripten
+) else if /i "%1"=="release" (
+    set native_config=release64
+    set wasm_config=releaseemscripten
+) else (
+    echo %0 ^<clean ^| debug ^| release^> 1>&2
+    exit /b 1
+)
+
+cd gmake && (
+    make config=!native_config! conway_geom_native webifc_native && make config=!wasm_config! conway_geom_wasm conway_geom_wasm_mt
+)
+
+cd ..
+
+if errorlevel 1 (
+    echo ! Build failed
+    exit /b 1
+) else (
+    echo Finished.
+    exit /b 0
+)
